b1757f8521ad1bc2e3111b0cb5cbd3e9fb252337
juies0608 <58469330+juies0608@users.noreply.github.com>
2020-05-07 21:43:45 +0800

    網頁連結

---
+++ CardGame/Assets/GetCard.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;  //引用 網路連線 API
+using System.Collections;
+
+
+public class GetCard : MonoBehaviour
+{
+  private IEnumerator GetCardData()
+    {
+        //引用 (網路要求 www= 網路要求.post("網址","")
+        using (UnityWebRequest www = UnityWebRequest.Post("https://script.google.com/macros/s/AKfycbzi5nYyqCyU8LEtut2vsxyzZtaDHnrvTGwJHCj2uS91gqOARVLu/exec", ""))
+        {
+            //等待 網路要求時間
+            yield return www.SendWebRequest();
+
+            if (www.isHttpError || www.isHttpError)
+            {
+                print("連線錯誤:" + www.error);
+            }
+            else
+            {
+                print(www.downloadHandler.text);
+            }
+        }
+    }
+    private void Start()
+    {
+        StartCoroutine(GetCardData());
+    }
+}
